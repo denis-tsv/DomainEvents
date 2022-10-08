7549ef8b0332a3f398c4db471230e9126f5f1a73
Denis <yadeniskotcvet@gmail.com>
2022-10-08 16:13:27 +0700

    Account group controller

---
+++ DomainEvents.Api/Controllers/AccountGroupsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using DomainEvents.UseCases.AccountGroups.Commands.RemoveAccountFromGroup;
+
+namespace DomainEvents.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AccountGroupsController : ControllerBase
+{
+    private readonly ISender _sender;
+
+    public AccountGroupsController(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    [HttpPost("{id}")]
+    public async Task RemoveAccount([FromRoute] int id, [FromBody] int accountId, CancellationToken cancellationToken)
+    {
+        await _sender.Send(new RemoveAccountFromGroupCommand { AccountGroupId = id, AccountId = accountId }, cancellationToken);
+    }
+}
