6b16c51db28e8118d9e27977590298dde95ce072
Denis <yadeniskotcvet@gmail.com>
2023-04-18 13:27:00 +0700

    add update product command

--- DomainEvents.Api/Controllers/ProductsController.cs
+++ DomainEvents.Api/Controllers/ProductsController.cs
@@ -15,7 +15,19 @@ public class ProductsController : ControllerBase
         _sender = sender;
     }
 
-    [HttpDelete("{id}")]
+    [HttpPost]
+    public async Task<int> Create(CancellationToken cancellationToken)
+    {
+        return await _sender.Send(new CreateProductCommand(), cancellationToken);
+    }
+
+    [HttpPut("{id:int}")]
+    public Task Update([FromRoute] int id, CancellationToken cancellationToken)
+    {
+        return _sender.Send(new UpdateProductCommand(id), cancellationToken);
+    }
+
+    [HttpDelete("{id:int}")]
     public async Task Delete([FromRoute] int id, CancellationToken cancellationToken)
     {
         await _sender.Send(new DeleteProductCommand(id), cancellationToken);
