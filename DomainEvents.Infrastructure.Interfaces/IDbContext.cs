cedb5733f205881281c8148c1ab4e961cf58164a
Denis <yadeniskotcvet@gmail.com>
2023-08-07 13:50:08 +0700

    Colg operation in external service

--- DomainEvents.Infrastructure.Interfaces/IDbContext.cs
+++ DomainEvents.Infrastructure.Interfaces/IDbContext.cs
@@ -4,8 +4,6 @@ namespace DomainEvents.Infrastructure.Interfaces;
 
 public interface IDbContext : IDbSets
 {
-    bool IsTransactionStarted { get; }
-
     Task<IDbContextTransaction> BeginTransactionAsync(CancellationToken cancellationToken);
 
     Task<int> SaveChangesAsync(CancellationToken cancellationToken);
