80c14df211c4cd42b1a984310d2a75f87ca0de76
Denis <yadeniskotcvet@gmail.com>
2023-08-07 13:30:09 +0700

    No command from command

--- DomainEvents.UseCases/Categories/Commands/RemoveProductFromCategoryCommand.cs
+++ DomainEvents.UseCases/Categories/Commands/RemoveProductFromCategoryCommand.cs
@@ -9,10 +9,12 @@ public record RemoveProductFromCategoryCommand(int ProductId, int CategoryId) : 
 public class RemoveProductFromCategoryCommandHandler : IRequestHandler<RemoveProductFromCategoryCommand>
 {
     private readonly IDbContext _dbContext;
+    private readonly CategoryService _categoryService;
 
-    public RemoveProductFromCategoryCommandHandler(IDbContext dbContext)
+    public RemoveProductFromCategoryCommandHandler(IDbContext dbContext, CategoryService categoryService)
     {
         _dbContext = dbContext;
+        _categoryService = categoryService;
     }
 
     public async Task Handle(RemoveProductFromCategoryCommand request, CancellationToken cancellationToken)
@@ -21,12 +23,7 @@ public class RemoveProductFromCategoryCommandHandler : IRequestHandler<RemovePro
             .Include(x => x.Products) //can't include filter because need to check products count
             .FirstOrDefaultAsync(x => x.Id == request.CategoryId, cancellationToken);
 
-        category!.RemoveProduct(request.ProductId);
-
-        if (!category.Products.Any())
-        {
-            _dbContext.Categories.Remove(category);
-        }
+        _categoryService.RemoveProductFromCategory(category!, request.ProductId);
 
         await _dbContext.SaveChangesAsync(cancellationToken);
     }
